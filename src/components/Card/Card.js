fafbc21e8021e05e2e9347bed65b747a149c0a83
Jeff Goodall <goodallhj@gmail.com>
2018-02-22 10:02:10 -0700

    setting up the Card files

---
+++ src/components/Card/Card.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
+export class Card extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  
+});
+
+const mapDispatchToProps = dispatch ({
+  
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Card)
