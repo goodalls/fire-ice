f9caf10887a66f31377985f5ad4b74a0492dada6
Jeff Goodall <goodallhj@gmail.com>
2018-02-22 14:03:10 -0700

    Adding App.js tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -5,40 +5,27 @@ import * as api from '../../api';
 import { ok } from 'assert';
 
 describe('APP', () => {
-//network request failed....
+  //network request failed....
 
-  // it('should match the snapshot', () => {
-  //   const wrapper = shallow(
-  //     <App houses={[]} populate={jest.fn()} fake={jest.fn()} fakeAction={jest.fn()} />
-  //   );
-  //   expect(wrapper).toMatchSnapshot();
-  // });
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<App houses={[]} populate={jest.fn()} />);
+    expect(wrapper).toMatchSnapshot();
+  });
   describe('RequestData', () => {
-    // window.fetch = jest.fn().mockImplementation(() =>
-    //   Promise.resolve({
-    //     result: ok,
-    //     json: () =>
-    //       Promise.resolve({
-    //         houses: [],
-    //         result: ok
-    //       })
-    //   })
-    // );
-
-    // it('should call api helper function with expected params', async() => {
-    //   const wrapper = shallow(<App houses={[]} populate={jest.fn()} />);
-    //   const mockURL = 'jeff.is.cool.com'
-    //   api.fetchParse = jest.fn(mockURL);
-    //   await wrapper.instance().requestData();
-    //   wrapper.update()
-    //   expect(api.fetchParse).toHaveBeenCalled();
-    //   // With(
-    //   //   'http://localhost:3001/api/v1/houses'
-    //   // );
-    // });
+    it('should call api helper function with expected params', async () => {
+      const wrapper = shallow(<App houses={[]} populate={jest.fn()} />);
+      const mockURL = 'jeff.is.cool.com';
+      api.fetchParse = jest.fn();
+      await wrapper.instance().requestData();
+      expect(api.fetchParse).toHaveBeenCalledWith(
+        'http://localhost:3001/api/v1/houses'
+      );
+    });
   });
   describe('housesRenderCheck', () => {
-    it('should', () => {});
+    it('should', () => {
+      const wrapper = shallow(<App houses={[]} populate={jest.fn()} />);
+    });
   });
 
   describe('MSTP and MDTP', () => {
